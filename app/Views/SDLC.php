6b6f6a65d59e593b006de8e9dce47c713d6fa359
Karen Lin <karen901208@gmail.com>
2023-06-01 15:08:54 +0800

    first commit

---
+++ app/Views/SDLC.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SDLC</title>
+</head>
+
+<body>
+    <h1><?= esc($heading) ?></h1>
+    <table>
+        <tr>
+            <th>Time Used</th>
+            <th>Step</th>
+            <th>Description</th>
+        </tr>
+        <?php foreach ($todo_list as $todo => $todo_num) : ?>
+            <tr>
+                <td><?= esc($todo) ?></td>
+                <?php foreach ($todo_num as $todo_item) : ?>
+                    <td><?= esc($todo_item) ?></td>
+                <?php endforeach ?>
+            </tr>
+        <?php endforeach ?>
+    </table>
+</body>
+
+</html>
